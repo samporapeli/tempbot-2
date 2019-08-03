5e53e91e95fb6f5e573bf6f9434f98662348284e
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2019-08-03 10:10:07 +0300

    Add feature: basic temperature fetching

---
+++ temp_fetch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+
+onewire_path = "/sys/bus/w1/devices/"
+
+def sensor_list():
+    sensors = os.listdir(onewire_path)
+    sensors.remove("w1_bus_master1")
+    return sensors
+
+def sensor_temp(sensor):
+    try:
+        file_path = "{path}{sensor}/w1_slave".format(
+            path = onewire_path, sensor = sensor)
+        with open(file_path) as sensor_file:
+            data = sensor_file.readlines()
+    except:
+        return "Error with accessing sensor data"
+    try:
+        # The temperature information is on the second line
+        # after "t="
+        second_line_elements = data[1].split(" ")
+        t = list(filter(lambda x: "t=" in x, second_line_elements))
+        # First element starting with "t=" is the right one
+        # (there should only be one)
+        millicelcius = float(t[0].split("=")[1])
+        return millicelcius / 1000
+    except:
+        return "Error with parsing temperature data"
