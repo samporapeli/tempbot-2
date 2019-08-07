398cc72185c87de81c61b8aec73e08f56a9e94f2
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2019-08-07 20:26:26 +0300

    Update config file to support temperatures.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-onewire_path = "/sys/bus/w1/devices/"
+import os
 
+onewire_path = "/sys/bus/w1/devices/"
+temperature_directory = os.path.expanduser("~") + "/tempbot-2/log/"
 sensor_names = {
     "28-0516b2fb91ff":"Ulko",
     "28-0516b2e398ff":"Serveri",
     "28-04169353faff":"Sisä",
     "28-0516b2fa75ff":"Lattia"
 }
-
 use_decimal_comma = True
+
