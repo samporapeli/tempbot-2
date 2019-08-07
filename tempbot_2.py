113ff339d63a1cc8687ad60995ef30be624d350d
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2019-08-07 20:54:31 +0300

    Add Telegram bot

---
+++ tempbot_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import time
+import bot
+from temperature import Temperatures
+
+tg = bot.TelegramBot()
+temperatures = Temperatures()
+
+while True:
+    message = tg.next_message()
+    if (message):
+        tg.reply(message, temperatures.to_string())
+    time.sleep(0.1)
